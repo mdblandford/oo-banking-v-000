4ef342afd1a86ad8ec1c6df35294cc4a6e9b45ef
Michael Blandford <mdblandford@gmail.com>
2018-06-27 10:17:39 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,23 +8,23 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
-  
+
   def deposit(money)
     @balance += money
   end
-  
+
   def display_balance
     "Your balance is $#{balance}."
   end
-  
+
   def valid?
-    if @status == "open" && @balance > 0 
+    if @status == "open" && @balance > 0
       true
     else
       false
     end
   end
-  
+
   def close_account
     self.status = "closed"
   end
